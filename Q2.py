2f8597c27f59110239a9a5307f03a2854caff469
Wallace <r05725039@ntu.edu.tw>
2017-08-23 15:40:21 +0800

    u

---
+++ Q2.py
@@ -0,0 +1,10 @@
+def x35(num):
+    o = []
+    a = [j for j in range(num)] 
+    for i in a:
+        if(i%3!=0):
+            if(i%5!=0):
+                o.append(i)
+        elif(i%3==0 and i%5==0):
+            o.append(i)
+    return len(o)
